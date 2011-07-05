e4f4fd5197069467ad86d86ecb1ec9cdc2dca955
Michael V. Battista <michael.v.battista@gmail.com>
2011-07-05 15:24:33 -0400

    test_generation subroutine complete.
    Signed-off-by: Michael V. Battista <michael.v.battista@gmail.com>

--- data_test.pl
+++ data_test.pl
@@ -4,19 +4,19 @@ use warnings;
 use Data::Dumper;
 
 sub test_generation {
-	my @models = @_;
-	my $file_count = 9000;
-	for my $table (@models){
-		my $file_name = $table->{class_name};
-		$file_name =~ s/::/_/g;
-		$file_name = $file_count.$file_name.'.t';
-		my @fields;
-		for my $i (@{$table->{fields}}) {
-     		push @fields, $i->{name};
- 	  	}
- 	  	$table->{class_name} =~ /^Fina::Corp(.+)$/;
- 	  	my $class_name = $1;
- 	  	my $s = <<TESTBEGIN;
+    my @models = @_;
+    my $file_count = 9000;
+    for my $table (@models){
+        my $file_name = $table->{class_name};
+        $file_name =~ s/::/_/g;
+        $file_name = $file_count.$file_name.'.t';
+#        my @fields;
+#        for my $i (@{$table->{fields}}) {
+#            push @fields, $i->{name};
+#        }
+        $table->{class_name} =~ /^Fina::Corp::(.+)$/;
+        my $class_name = $1;
+        my $s = <<"TEST1";
 #!/usr/local/bin/perl
 #
 #
@@ -30,18 +30,47 @@ our \$class;
 BEGIN {
     my \$base_class = 'Fina::Corp';
     \$class = \$base_class.'::$class_name';
-    use_ok( \$class);
+    use_ok(\$class);
 }
 
 isa_ok(\$class, 'Rose::DB::Object');
 
-my @columns = qw(
+my \@columns = qw(
+TEST1
+        for my $col (@{$table->{fields}}) {
+            $s .= '    ';
+            $s .= $col->{name};
+            $s .= "\n";
+        }
+        $s .= ");\n\nmy \@foreign_keys = qw(\n";
+        for my $col (@{$table->{constraints}}) {
+            $s .= '    ';
+            $s .= $col->{model_name};
+            $s .= "\n";
+        }
+        $s .= ");\n\nmy \@relationships = qw(\n";
+        for my $col (@{$table->{relationships}}) {
+            $s .= '    ';
+            $s .= $col->{name};
+            $s .= "\n";
+        }
+        $s .= <<'TEST2';
+);
+push @relationships, @foreign_keys;
 
- 	  	TESTBEGIN
- 	  	for my $col (@fields) {
- 	  		$s =. 
- 	  	}
-	}
+my @methods = qw{
+    manage_description
+};
+
+can_ok($class, ( @methods ));
+is_deeply([$class->meta->column_names], \@columns, "Columns Defined as Expected");
+is_deeply([ sort (map { $_->name } @{$class->meta->foreign_keys}) ], [sort @foreign_keys], "Foreign Keys Defined as Expected");
+is_deeply([ sort (map { $_->name } @{$class->meta->relationships}) ], [sort @relationships], "Relationships Defined as Expected");
+
+TEST2
+    return $s;
+    }
+
 }
 
 sub sql_generation {
@@ -52,15 +81,15 @@ sub sql_generation {
     my @fields;
     my @triggers;
     for my $table (@models) {
-    	push @sequences, @{$table->{sequences}} if (defined @{$table->{sequences}});
-    	push @triggers, @{$table->{triggers}} if (defined @{$table->{triggers}});
-    	for my $i (@{$table->{fields}}) {
-     		push @fields, $i->{name};
- 	  	}
+        push @sequences, @{$table->{sequences}} if (defined @{$table->{sequences}});
+        push @triggers, @{$table->{triggers}} if (defined @{$table->{triggers}});
+        for my $i (@{$table->{fields}}) {
+            push @fields, $i->{name};
+        }
     }
-    print "Hi there\n\n";
+#    print "Hi there\n\n";
     
-    print Dumper(\@sequences, \@triggers, \@fields);
+#    print Dumper(\@sequences, \@triggers, \@fields);
 
     if ( @sequences ) {
         $s .= "\\echo Creating sequences.\n\n";
@@ -122,8 +151,8 @@ sub sql_generation {
             }
         }
         for my $i (@{$table->{constraints}}) {
-            my $str = "    CONSTRAINT fk_".$i->{field_name}." FOREIGN KEY (".$i->{field_name}.
-                ") REFERENCES ".$i->{fk_table}." (".$i->{fk_field}.") MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE";
+            my $str = "    CONSTRAINT fk_".$i->{field}." FOREIGN KEY (".$i->{field}.
+                ") REFERENCES ".$i->{sql_table}." (".$i->{sql_column}.") MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE";
             unless ($comma_count == 0) {
                 $str .= ",\n";
                 $comma_count--;
@@ -138,7 +167,7 @@ sub sql_generation {
     $s .= "\\echo Table creation complete.\n\n";
 
     if (scalar @triggers > 0) {
-    	print "\n\nThis is a trigger!\n\n";
+#        print "\n\nThis is a trigger!\n\n";
         $s .= "\\echo Creating triggers.\n\n";
         for my $i (@triggers) {
             my $str = "CREATE TRIGGER ".$i->{name}."\n    BEFORE INSERT OR UPDATE ON "
@@ -176,16 +205,19 @@ push @{$table{fields}}, {name => 'application_name', type => 'VARCHAR', not_null
 push @{$table{fields}}, {name => 'action', type => 'BOOLEAN', not_null => 1, default => 'false', };
 push @{$table{fields}}, {name => 'message', type => 'VARCHAR', not_null => 1 };
 
-push @{$table{constraints}}, {field_name => 'alert_level_id', fk_field => 'id',
-    fk_table => 'alert_levels', m_class => 'Fina::Corp::M::Alert::Level', m_name => 'alert_levels'
+push @{$table{constraints}}, {field => 'alert_level_id', sql_column => 'id',
+    sql_table => 'alert_levels', model_name => 'Fina::Corp::M::Alert::Level', model_name => 'alert_levels'
     };
 push @{$table{triggers}}, {name => 'alerts_last_modified', table => 'alerts'};
 
-print Dumper(\%table);
+#print Dumper(\%table);
 my $sql_string = sql_generation(\%table);
+my $test_str = test_generation(\%table);
 
 print "Here is the SQL string: $sql_string";
 
-open my $out_fh, '>', "data_test_out.sql" or die "Cannot create output file: $!";
-print $out_fh $sql_string;
-close $out_fh or die "Cannot close output file: $!";
+#open my $out_fh, '>', "data_test_out.sql" or die "Cannot create output file: $!";
+#print $out_fh $sql_string;
+#close $out_fh or die "Cannot close output file: $!";
+
+print $test_str;
